64ac68fac8db90c7f42ddcce6bc4ef182444ea33
debrah_kobby <bessahsam0277@gmail.com>
2025-06-13 00:25:37 +0000

    Javascript Calculator

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-//DOM
-let errorMessage = document.getElementById("errormessage");
+let num_1 = document.getElementById("num_1");
+let num_2 = document.getElementById("num_2");
 
-//functions
+let result = document.getElementById("results");
 
-function purchaseBtn() {
-  errorMessage.textContent = "Something went wrong please try again";
+let assignedNum_1 = (document.getElementById("num_1").textContent =9 );
+
+let assignedNum_2 = (document.getElementById("num_2").textContent =7 );
+
+function addBtn() {
+  let addition = Number(assignedNum_1) + Number(assignedNum_2);
+  result.textContent = "Sum : " + addition;
+}
+
+function subtractBtn() {
+  let subtraction = Number(assignedNum_1) - Number(assignedNum_2);
+  result.textContent = "Difference: " + subtraction;
+}
+
+function divideBtn() {
+  let division = Number(assignedNum_1) / Number(assignedNum_2);
+  result.textContent = "Quotient: " + division;
+}
+
+function multiplyBtn() {
+  let multiplication = Number(assignedNum_1) * Number(assignedNum_2);
+  result.textContent = "Product: " + multiplication;
 }
