801a29ea7ed60ef7ee5ee6e7e1a7eb0aa090408e
Prince Sam <bessahsam0277@gmail.com>
2025-06-18 16:30:10 -0700

    BlackJack Game 1.0

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-//DOM
-let countEl = document.getElementById("count-el");
-let saveEl = document.getElementById("save-el");
+let firstCard = 17;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let cardsOuput = "1st Card - " + firstCard + ", " + "2nd Card - " + secondCard;
 
-//VARIABLES
-let count = 0;
-
-//FUNCTIONS
-function increment() {
-  count += 1;
-  countEl.textContent = count;
-  console.log(count);
+function startGame() {
+  if (sum <= 20) {
+    message = "Do u want to draw another card";
+  } else if (sum === 21) {
+    message = "Wohooo! You got BlackJack";
+    hasBlackJack = true;
+  } else {
+    message = "Youre out of the Game";
+    isAlive = false;
+  }
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + cardsOuput;
+  messageEl.textContent = message;
 }
-function decrement() {
-  count -= 1;
-  countEl.textContent = count;
-  console.log(count);
-}
 
-function saveBtn() {
-  let countstr = count + " - ";
-  saveEl.textContent += countstr;
-  countEl.textContent = 0;
-  count = 0;
-}
+function resetBtn() {
+  message = "Want to play a round?";
 
-function clearBtn() {
-  saveEl.textContent = "Previous Entries: ";
+  hasBlackJack = false;
+  isAlive = true;
+  messageEl.textContent = message;
+  cardsEl.textContent = "Cards: 0";
+  sumEl.textContent = "Sum: 0";
 }
--- tutorial.js
+++ tutorial.js
@@ -365,4 +365,33 @@ function funcForWelcome() {
   welcomePg.innerText = nameAndGreetings;
   console.log(welcomePg); }*/
 
-console.log(" Welcome to Kalcart, Prince");
+//DOM
+/* let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el"); */
+
+//VARIABLES
+/* let count = 0; */
+
+//FUNCTIONS
+/* function increment() {
+  count += 1;
+  countEl.textContent = count;
+  console.log(count);
+}
+function decrement() {
+  count -= 1;
+  countEl.textContent = count;
+  console.log(count);
+}
+
+function saveBtn() {
+  let countstr = count + " - ";
+  saveEl.textContent += countstr;
+  countEl.textContent = 0;
+  count = 0;
+}
+
+function clearBtn() {
+  saveEl.textContent = "Previous Entries: ";
+}
+ */
